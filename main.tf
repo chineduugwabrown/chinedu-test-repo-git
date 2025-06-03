e8f5c1775649d1ce42c4790fce8757bd2a212d56
chinedu <chinedu.ugwabrown@gmail.com>
2025-06-02 20:03:41 -0400

    gcs

--- main.tf
+++ main.tf
@@ -128,4 +128,14 @@
 #  }
 #  
 #}
+*/
 
+resource "google_storage_bucket" "gcs-bucket" {
+  name          = "image-store"
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  
+}
